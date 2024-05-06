222696af0fa55383f8ac051cde103881481af1cc
Archit <jainarchit1604@gmail.com>
2024-05-06 14:47:54 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
 
-import stripeLogo from "./assets/images/trusted-by/stripe.svg";
-import datastaxLogo from "./assets/images/trusted-by/datastax.svg";
-import cokeLogo from "./assets/images/trusted-by/coke.svg";
-import gmLogo from "./assets/images/trusted-by/gm.svg";
-import sharesLogo from "./assets/images/trusted-by/shares.svg";
-import shopifyLogo from "./assets/images/trusted-by/shopify.svg";
-import agencyElevationLogo from "./assets/images/trusted-by/agency-elevation.svg";
-import gitlabLogo from "./assets/images/trusted-by/gitlab.svg";
-import duolingoLogo from "./assets/images/trusted-by/duolingo.svg";
-import coyoteLogo from "./assets/images/trusted-by/coyote.svg";
-import astratoLogo from "./assets/images/trusted-by/astrato.svg";
-import mercadoLogo from "./assets/images/trusted-by/mercado.svg";
-
-import portfolioTemplateImg from "./assets/images/work/portfolio-template.webp";
-import agencyElevationImg from "./assets/images/work/agency-elevation.webp";
-import contributeDevImg from "./assets/images/work/contribute-dev.webp";
-
-import gitHub from "./assets/images/social-links/github.svg";
-
-import reactImg from "./assets/images/skills/react.webp";
-import svelteImg from "./assets/images/skills/svelte.webp";
-import typescriptImg from "./assets/images/skills/typescript.webp";
-import accessibilityImg from "./assets/images/skills/a11y.webp";
-import shellImg from "./assets/images/skills/shell.webp";
-import nextJsImg from "./assets/images/skills/next-js.webp";
-import graphqlImg from "./assets/images/skills/graphql.webp";
-import gitImg from "./assets/images/skills/git.webp";
-import cssImg from "./assets/images/skills/html.webp";
-import jsImg from "./assets/images/skills/js.webp";
-
-import richHarrisImage from "./assets/images/testimonials/rich-harris.png";
-import frankMeadImage from "./assets/images/testimonials/frank-mcsherry.webp";
-import brianHirshImage from "./assets/images/testimonials/brian-hirsh.png";
-import adamImage from "./assets/images/testimonials/adam.png";
-import valHeadImage from "./assets/images/testimonials/val-head.png";
-import timSatterwhiteImage from "./assets/images/testimonials/tim-satterwhite.webp";
-
-import githubLogo from "./assets/images/social-links/github.svg";
-import codepenLogo from "./assets/images/social-links/codepen.svg";
-import linkedinLogo from "./assets/images/social-links/linkedin.svg";
-import twitterLogo from "./assets/images/social-links/twitter.svg";
-
 import Client from "./components/Client";
 import WorkSection from "./components/WorkSection";
 import Skills from "./components/Skills";
@@ -59,9 +17,6 @@ export default function App() {
     setNavHidden(!navHidden);
     document.body.classList.toggle("lock-screen");
   };
-
-  const workElsRef = useRef([]);
-  const workImgsRef = useRef([]);
 
   useEffect(() => {
     document.body.addEventListener("keydown", (e) => {
